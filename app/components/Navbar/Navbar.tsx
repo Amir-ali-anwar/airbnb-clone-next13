4cb99c772f6115bb4b22334a650078fc7828348a
Amir Ali anwar <amiralianwar611@gmail.com>
2023-05-07 10:35:03 +0500

    initial setup

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Navbar({ }: Props) {
+    return (
+        <div className='fixed w-full bg-white z-10 shadow-sm'>
+            <div className='py-4 border-b-[1px]'>
+
+            </div>
+        </div>
+    )
+}
