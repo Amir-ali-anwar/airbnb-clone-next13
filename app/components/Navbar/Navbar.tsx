bbeae4cd5351661a8ee9dbce5a22e5e97143086c
Amir Ali anwar <amiralianwar611@gmail.com>
2023-05-07 10:57:47 +0500

    added logo,navbar and container component

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import Container from '../Container'
+import Logo from './Logo'
 type Props = {}
 
 export default function Navbar({ }: Props) {
     return (
         <div className='fixed w-full bg-white z-10 shadow-sm'>
             <div className='py-4 border-b-[1px]'>
+                <Container>
+                <Logo />
 
+                </Container>
             </div>
         </div>
     )
