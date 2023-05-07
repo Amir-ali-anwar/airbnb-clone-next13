b7737d58c10205417f1fd6bd6dc8ea95e04c6f3a
Amir Ali anwar <amiralianwar611@gmail.com>
2023-05-07 11:41:12 +0500

    added menu item and user menu components

---
+++ app/components/Navbar/MenuItem.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+type Props = {}
+
+const MenuItem = (props: Props) => {
+  return (
+    <div>MenuItem</div>
+  )
+}
+
+export default MenuItem
---
+++ app/components/Navbar/UserMenu.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+type Props = {}
+
+const UserMenu = (props: Props) => {
+    return (
+        <div>UserMenu</div>
+    )
+}
+
+export default UserMenu
